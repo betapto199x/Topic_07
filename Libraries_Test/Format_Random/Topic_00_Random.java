0769c9d7cd82cddb7158e29bc78368a951894f58
Luong Anh Huy <luonganhhuy5@gmail.com>
2023-07-25 00:01:50 +0700

    Random

---
+++ Libraries_Test/Format_Random/Topic_00_Random.java
@@ -0,0 +1,13 @@
+package Format_Random;
+
+import java.util.Random;
+
+public class Topic_00_Random {
+
+	public static void main(String[] args) {
+		Random rand = new Random();
+		rand.nextInt(999999);
+
+	}
+
+}
